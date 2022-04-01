24b0495985cbf7aa7f8cb9818abe87b83e3156ed
AlexanderFrolov <sovereignty.alex@gmail.com>
2022-04-01 20:59:51 +0300

    added map routing, added custom middleware for logs (write in console and file).  added use static files for path: Views.

---
+++ ASP_App/ASP_App/ASP_App/Middlewares/LoggingMidlleware.cs
@@ -0,0 +1,59 @@
+﻿namespace ASP_App.Middlewares
+{
+    public class LoggingMidlleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        ///  Middleware component must have a constructor that accepts RequestDelegate
+        /// </summary>
+        public LoggingMidlleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        ///  Invoke or InvokeAsync must be implemented
+        /// </summary>
+        public async Task InvokeAsync(HttpContext context, IWebHostEnvironment env)
+        {
+            string logMessage = CreateLogMessage(context);
+
+            LogConsole(logMessage);
+
+            await LogFile(context, logMessage, env);
+
+            // send request further by pipeline
+            await _next.Invoke(context);
+        }
+
+        /// <summary>
+        ///   create string to write to log
+        /// </summary>
+        private string CreateLogMessage(HttpContext context)
+        {
+            return $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path} " +
+            $"{Environment.NewLine}";
+        }
+
+        /// <summary>
+        /// logging to console
+        /// </summary>
+        private void LogConsole(string logMessage)
+        {
+            Console.Write(logMessage);
+        }
+
+        /// <summary>
+        ///  write to log file 
+        /// </summary>    
+        private async Task LogFile(HttpContext context, string logMessage, IWebHostEnvironment env)
+        {
+            // Path to the log (again, we use the IWebHostEnvironment properties)
+            string logFilePath = Path.Combine(env.ContentRootPath, "Logs", "RequestLog.txt");
+
+            // use asynchronous write to file
+            await File.AppendAllTextAsync(logFilePath, logMessage);
+        }
+    }
+}
--- ASP_App/ASP_App/ASP_App/Program.cs
+++ ASP_App/ASP_App/ASP_App/Program.cs
@@ -1,7 +1,70 @@
+using ASP_App.Middlewares;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.Environment.WebRootPath = "Views";
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "Views"))
+});
+
+app.UseRouting();
+
+// write logs into console and txt file
+app.UseMiddleware<LoggingMidlleware>();
+
+// route for root page
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync($"Welcome to the {app.Environment.ApplicationName}!");
+    });
+});
+
+// other routing 
+app.Map("/about", builder => About(builder, app.Environment));
+app.Map("/config", builder => Config(builder, app.Environment));
+
+// if route not found
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Page not found!  Hello from non-Map delegate. <p>");
+});
 
 app.Run();
-//
+
+/// <summary>
+///  Config page handler
+/// </summary>
+static void Config(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync($"App name: {env.ApplicationName}. App running configuration: {env.EnvironmentName}");
+    });
+}
+
+/// <summary>
+///  About page handler
+/// </summary>
+static void About(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync($"{env.ApplicationName} - ASP.Net Core tutorial project");
+    });
+}
